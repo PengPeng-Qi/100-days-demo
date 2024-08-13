89df342b8fba10869147997333014aa3874231da
pengpengq <1747447034@qq.com>
2024-08-13 13:51:43 +0800

    feat: update layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,15 @@ export default function Home() {
       <div className="font-semibold">A Next.js With TailwindCss Starter Template</div>
 
       <div className="text mt-12">
-        <Button variant="link" className="px-0">
-          <Link href="demo/logo">Logo 动画</Link>
+        <Button variant="link" className="pl-0">
+          <Link href="/demo/logo">Logo 动画</Link>
         </Button>
-        <div>
-          <Button variant="link" className="px-0">
-            <Link href="demo/dark">dark mode switch</Link>
-          </Button>
-        </div>
-        <div>
-          <Button variant="link" className="px-0">
-            <Link href="demo/background">背景动画</Link>
-          </Button>
-        </div>
+        <Button variant="link">
+          <Link href="/demo/dark">dark mode switch</Link>
+        </Button>
+        <Button variant="link">
+          <Link href="/demo/background">背景动画</Link>
+        </Button>
       </div>
     </div>
   );
